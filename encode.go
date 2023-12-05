e8116929867103d8117a87d2adf63f846f3ccc77
pandoratoolbox <30897811+pandoratoolbox@users.noreply.github.com>
2023-12-06 06:16:10 +0700

    add zero is null option

--- encode.go
+++ encode.go
@@ -337,32 +337,64 @@ func (e *encodeState) error(err error) {
 	panic(jsonError{err})
 }
 
+var ZeroIsNull bool = true
+
 func isEmptyValue(v reflect.Value) bool {
-	switch v.Kind() {
-	case reflect.Array, reflect.Map, reflect.Slice, reflect.String:
-		// return v.Len() == 0
-	case reflect.Bool:
-		// return !v.Bool()
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		// return v.Int() == 0
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		// return v.Uint() == 0
-	case reflect.Float32, reflect.Float64:
-		// return v.Float() == 0
-	case reflect.Interface, reflect.Ptr:
-		return v.IsNil()
-	case reflect.Struct:
-		vt := v.Type()
-		for i := v.NumField() - 1; i >= 0; i-- {
-			if vt.Field(i).PkgPath != "" {
-				continue // Private field
+	switch ZeroIsNull {
+	case true:
+		switch v.Kind() {
+		case reflect.Array, reflect.Map, reflect.Slice, reflect.String:
+			return v.Len() == 0
+		case reflect.Bool:
+			return !v.Bool()
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			return v.Int() == 0
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+			return v.Uint() == 0
+		case reflect.Float32, reflect.Float64:
+			return v.Float() == 0
+		case reflect.Interface, reflect.Ptr:
+			return v.IsNil()
+		case reflect.Struct:
+			vt := v.Type()
+			for i := v.NumField() - 1; i >= 0; i-- {
+				if vt.Field(i).PkgPath != "" {
+					continue // Private field
+				}
+				if !isEmptyValueOrZero(v.Field(i)) {
+					return false
+				}
 			}
-			if !isEmptyValueOrZero(v.Field(i)) {
-				return false
-			}
+			return true
 		}
-		return true
+	case false:
+		switch v.Kind() {
+		case reflect.Array, reflect.Map, reflect.Slice, reflect.String:
+			// return v.Len() == 0
+		case reflect.Bool:
+			// return !v.Bool()
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			// return v.Int() == 0
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+			// return v.Uint() == 0
+		case reflect.Float32, reflect.Float64:
+			// return v.Float() == 0
+		case reflect.Interface, reflect.Ptr:
+			return v.IsNil()
+		case reflect.Struct:
+			vt := v.Type()
+			for i := v.NumField() - 1; i >= 0; i-- {
+				if vt.Field(i).PkgPath != "" {
+					continue // Private field
+				}
+				if !isEmptyValueOrZero(v.Field(i)) {
+					return false
+				}
+			}
+			return true
+		}
 	}
+
 	return false
 }
 
